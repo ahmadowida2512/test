a00ed9ad2820e7bb60d21777d2760c617078039d
ahmadowida2512 <aowida2512@gmail.com>
2024-11-26 16:20:41 +0200

    اخرررررررررررررررررر تاسك

---
+++ Assets/script/s20/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+namespace aa
+{
+    public class GameObject : MonoBehaviour
+    {
+       public GameObject[] spheres;
+        void Update()
+        {
+          
+          
+          
+          float displacement = 1f;
+        foreach (GameObject sphere in spheres)
+        {
+
+            displacement +=0.5f;
+            float yNew = Mathf.Sin(Time.time) + displacement;
+            sphere.transform.localScale = new Vector3(yNew, yNew, sphere.transform.localScale.z);
+
+            float xNew = Mathf.Cos(Time.time);
+            sphere.transform.position += new Vector3(0, 0, xNew * Time.deltaTime);
+
+        }
+
+
+        }
+    }
+}
